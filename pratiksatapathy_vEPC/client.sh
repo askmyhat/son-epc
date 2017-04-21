c3d2febe5c91eb1b8677e32c246e0b1f744db0c3
Janos Elek <janos.elek@intec.ugent.be>
2017-04-21 03:52:21 +0200

    Add ran and sink to compose

--- pratiksatapathy_vEPC/client.sh
+++ pratiksatapathy_vEPC/client.sh
@@ -1,11 +1,16 @@
-hss_mgmt=`sudo docker inspect hss_pp | jq '.[0].NetworkSettings.Networks.pratiksatapathyvepc_default.IPAddress' | sed 's/"//g'`
+function getIp {
+  inspect=`sudo docker inspect $1`
+  echo $inspect | jq '.[0].NetworkSettings.Networks | map(.IPAddress)[0]' | sed 's/"//g'
+}
+
+hss_mgmt=`getIp hss_pp`
 hss_data=$hss_mgmt
-mme_mgmt=`sudo docker inspect mme_pp | jq '.[0].NetworkSettings.Networks.pratiksatapathyvepc_default.IPAddress' | sed 's/"//g'`
+mme_mgmt=`getIp mme_pp`
 mme_data=$mme_mgmt
-pgw_mgmt=`sudo docker inspect pgw_pp | jq '.[0].NetworkSettings.Networks.pratiksatapathyvepc_default.IPAddress' | sed 's/"//g'`
+pgw_mgmt=`getIp pgw_pp`
 pgw_data=$pgw_mgmt
-sgw_mgmt_1=`sudo docker inspect sgw_pp_1 | jq '.[0].NetworkSettings.Networks.pratiksatapathyvepc_default.IPAddress' | sed 's/"//g'`
-sgw_mgmt_2=`sudo docker inspect sgw_pp_2 | jq '.[0].NetworkSettings.Networks.pratiksatapathyvepc_default.IPAddress' | sed 's/"//g'`
+sgw_mgmt_1=`getIp sgw_pp_1`
+sgw_mgmt_2=`getIp sgw_pp_2`
 sgw_data_1=$sgw_mgmt_1
 sgw_data_2=$sgw_mgmt_2
 hss_host=`sudo docker inspect hss_pp | jq '.[0].Config.Hostname' | sed 's/"//g'`
@@ -15,7 +20,10 @@ sgw_host_1=`sudo docker inspect sgw_pp_1 | jq '.[0].Config.Hostname' | sed 's/"/
 sgw_host_2=`sudo docker inspect sgw_pp_2 | jq '.[0].Config.Hostname' | sed 's/"//g'`
 ds_ip=$hss_data
 local_ip=`sudo docker inspect hss_pp | jq '.[0].NetworkSettings.Networks | map(.Gateway)[0]' | sed 's/"//g'`
-lb_ip=`sudo docker inspect lb_pp | jq '.[0].NetworkSettings.Networks | map(.IPAddress)[0]' | sed 's/"//g'`
+lb_ip=`getIp lb_pp`
+sink_ip=`getIp sink_pp`
+ransim_1=`getIp ransim_pp_1`
+ransim_2=`getIp ransim_pp_2`
 
 echo "HSS: $hss_host: $hss_mgmt ($hss_data)"
 echo "MME: $mme_host: $mme_mgmt ($mme_data)"
@@ -24,12 +32,15 @@ echo "SGW_2: $sgw_host_2: $sgw_mgmt_2 ($sgw_data_2)"
 echo "PGW: $pgw_host: $pgw_mgmt ($pgw_data)"
 echo "local_ip: $local_ip"
 echo "lb_ip: $lb_ip"
+echo "sink_ip: $sink_ip"
+echo "ransim_1: $ransim_1"
+echo "ransim_2: $ransim_2"
 
 args="--hss_mgmt $hss_mgmt --hss_data $hss_data"
 args+=" --mme_mgmt $mme_mgmt --mme_data $mme_data"
 args+=" --spgw_mgmt $sgw_mgmt_1,$sgw_mgmt_2"
 args+=" --spgw_data $sgw_data_1,$sgw_mgmt_2"
-args+=" --pgw_mgmt $pgw_mgmt --sgw_s5_ip $sgw_data_1,$sgw_data_2 --pgw_s5_ip $pgw_data --sink_ip $pgw_mgmt --pp"
+args+=" --pgw_mgmt $pgw_mgmt --sgw_s5_ip $sgw_data_1,$sgw_data_2 --pgw_s5_ip $pgw_data --sink_ip $sink_ip --pp"
 args+=" --mme_s1_ip $mme_data"
 args+=" --spgw_s1_ip $sgw_data_1,$sgw_data_2 --spgw_sgi_ip $pgw_data"
 args+=" --ds_ip $ds_ip --trafmon_ip $local_ip"
