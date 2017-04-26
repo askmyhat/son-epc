8baa35f9233426097a7cdf79a306773b1b1c2eac
Janos Elek <janos.elek@intec.ugent.be>
2017-04-26 10:38:04 +0200

    Updated OAI images

--- oai/client.sh
+++ oai/client.sh
@@ -1,21 +1,31 @@
-hss_mgmt=`sudo docker inspect hss | jq '.[0].NetworkSettings.Networks.sonepc_default.IPAddress' | sed 's/"//g'`
+function getIp {
+  sudo docker inspect $1 | jq '.[0].NetworkSettings.Networks | map(.IPAddress)[0]' | sed 's/"//g'
+}
+
+function getHost {
+  sudo docker inspect $1 | jq '.[0].Config.Hostname' | sed 's/"//g'
+}
+
+hss_mgmt=`getIp hss`
 hss_data=$hss_mgmt/24
-mme_mgmt=`sudo docker inspect mme | jq '.[0].NetworkSettings.Networks.sonepc_default.IPAddress' | sed 's/"//g'`
+mme_mgmt=`getIp mme`
 mme_data=$mme_mgmt/24
-spgw_mgmt=`sudo docker inspect spgw | jq '.[0].NetworkSettings.Networks.sonepc_default.IPAddress' | sed 's/"//g'`
+spgw_mgmt=`getIp spgw`
 spgw_data=$spgw_mgmt/24
-hss_host=`sudo docker inspect hss | jq '.[0].Config.Hostname' | sed 's/"//g'`
-mme_host=`sudo docker inspect mme | jq '.[0].Config.Hostname' | sed 's/"//g'`
-spgw_host=`sudo docker inspect spgw | jq '.[0].Config.Hostname' | sed 's/"//g'`
+hss_host=`getHost hss`
+mme_host=`getHost mme`
+spgw_host=`getHost spgw`
 
-echo "$hss_host: $hss_mgmt ($hss_data)"
-echo "$mme_host: $mme_mgmt ($mme_data)"
-echo "$spgw_host: $spgw_mgmt ($spgw_data)"
+echo "HSS: $hss_host: $hss_mgmt ($hss_data)"
+echo "MME: $mme_host: $mme_mgmt ($mme_data)"
+echo "SPGW: $spgw_host: $spgw_mgmt ($spgw_data)"
 
-args="--hss_mgmt $hss_mgmt --hss_data $hss_data"
-args+=" --mme_mgmt $mme_mgmt --mme_data $mme_data"
-args+=" --spgw_mgmt $spgw_mgmt --spgw_data $spgw_data"
-args+=" --hss_host $hss_host --mme_host $mme_host --spgw_host $spgw_host"
+args="--hss_mgmt_ip $hss_mgmt --hss_s6a_ip $hss_data"
+args+=" --mme_mgmt_ip $mme_mgmt --mme_s11_ip $mme_data"
 args+=" --mme_s1_ip $mme_data"
-args+=" --spgw_s1_ip $spgw_data --spgw_sgi_ip $spgw_data"
-son-vm-client $args $@
+args+=" --sgw_s1_ip $spgw_data --pgw_sgi_ip $spgw_data"
+
+spec_args="--hss_host $hss_host --mme_host $mme_host --spgw_host $spgw_host"
+spec_args+=" --spgw_mgmt_ip $spgw_mgmt --spgw_s11_ip $spgw_data"
+set -x
+son-vm-client $args $@ oai $spec_args
--- oai/hss.sh
+++ oai/hss.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-service mysql restart
+service mysql start
+/root/openair-cn/SCRIPTS/hss_db_create localhost root hurka root hurka oai_db
 son-vm-server -c server.conf -v
